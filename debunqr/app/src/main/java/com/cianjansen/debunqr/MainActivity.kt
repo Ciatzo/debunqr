c41cf486f9b28279b2c8fc5ac7c877d3e7cb243d
Cian Jansen <c.p.jansen@student.tudelft.nl>
2020-09-18 15:33:48 +0200

    Add working sandbox API call

--- debunqr/app/src/main/java/com/cianjansen/debunqr/MainActivity.kt
+++ debunqr/app/src/main/java/com/cianjansen/debunqr/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.cianjansen.debunqr
 
 import android.os.Bundle
-import android.util.JsonReader
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.google.gson.JsonObject
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.Response
-import java.io.StringReader
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,35 +25,55 @@ class MainActivity : AppCompatActivity() {
         testButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 println("click")
-                testHttpReq()
+                getNewSandboxApiKey()
             }
         })
 
     }
 
-    private fun testHttpReq() {
-        val client = OkHttpClient()
-        val request: Request = Request.Builder()
-            .url("https://public-api.sandbox.bunq.com/v1/sandbox-user")
-            .post(RequestBody.create(null, ByteArray(0)))
-            .addHeader("x-bunq-client-request-id", "1234")
-            .addHeader("cache-control", "no-cache")
-            .addHeader("x-bunq-geolocation", "0 0 0 0 NL")
-            .addHeader("x-bunq-language", "en_US")
-            .addHeader("x-bunq-region", "en_US")
-            .build()
-        val response: Response = client.newCall(request).execute()
-        val responseString: String = response.body().string()
-        println("Response:")
-        println(responseString)
-        val jsonObject: JsonObject = Gson().fromJson(responseString, JsonObject::class.java)
-        val apiKEy: JsonObject =
-            jsonObject.getAsJsonArray("Response").get(0).getAsJsonObject()
-                .get("Apikey").getAsJsonObject()
-        println("Key: $apiKEy")
+    /**
+     *
+     */
+    private fun getNewSandboxApiKey() {
+
+        object : Thread() {
+            override fun run() {
+                val client = OkHttpClient()
+                val request: Request = Request.Builder()
+                    .url("https://public-api.sandbox.bunq.com/v1/sandbox-user-person")
+                    .post(RequestBody.create(null, ByteArray(0)))
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("user-agent", "curl-request")
+                    .addHeader("x-bunq-client-request-id", "12121212")
+                    .addHeader("cache-control", "none")
+                    .addHeader("x-bunq-geolocation", "0 0 0 0 000")
+                    .addHeader("x-bunq-language", "nl_NL")
+                    .addHeader("x-bunq-region", "nl_NL")
+                    .build()
+                try {
+                    val response: Response = client.newCall(request).execute()
+                    println(response.code())
+                    val responseString: String = response.body().string()
+                    print("Response:")
+                    println(responseString)
+                    val jsonObject: JsonObject = Gson().fromJson(responseString, JsonObject::class.java)
+                    val apiKey: String =
+                        jsonObject.getAsJsonArray("Response").get(0).getAsJsonObject()
+                            .get("ApiKey").getAsJsonObject().get("api_key").toString()
+                    println("Key: $apiKey")
+
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+
+            }
+        }.start()
+
+
 
 
     }
+
 
 
 }
